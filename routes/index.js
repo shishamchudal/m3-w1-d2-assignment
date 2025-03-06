e8c3e292fb37e5e0b72321c1d280f213e026e167
Shisham Chudal <shishamchudal61@gmail.com>
2025-03-06 15:42:08 -0800

    Add Express server setup and registration form routes

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const { check, validationResult } = require("express-validator");
+
+router.get("/", function (req, res) {
+  res.render("form", { title: "Registration Form" });
+});
+
+router.post(
+  "/",
+  [
+    check("name").isLength({ min: 1 }).withMessage("Please enter a name"),
+    check("email").isLength({ min: 1 }).withMessage("Please enter an email"),
+  ],
+  function (req, res) {
+    // console.log(req.body);
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+      res.send("Thank you for your registration!");
+    } else {
+      res.render("form", {
+        title: "Registration Form",
+        errors: errors.array(),
+        data: req.body,
+      });
+    }
+  }
+);
+
+module.exports = router;
